939cb9929cfc08f4810ac4360ede8c58f63e20ad
Jonathan Curtis <jontycurtis@gmail.com>
2024-09-24 10:25:35 +0100

    more

--- src/views/HallView.vue
+++ src/views/HallView.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref, computed } from 'vue'
-import { useFloating, flip, shift, arrow, autoPlacement } from '@floating-ui/vue'
+import { ref, computed, Ref, watch } from 'vue'
+import {
+  useFloating,
+  flip,
+  shift,
+  arrow,
+  autoPlacement,
+  VirtualElement,
+  ClientRectObject,
+  MaybeElement,
+  computePosition
+} from '@floating-ui/vue'
 import { useResizeObserver } from '@vueuse/core'
 
 export type Dimensions = {
@@ -10,72 +20,143 @@ export type Dimensions = {
   right: number
 }
 
-export type Booth = {
+export type Booth = Dimensions & {
   id: string
   name: string
-  dimensions: Dimensions
 }
 
-export type HallData = {
+export type Hall = {
   width: number
   height: number
   booths: Booth[]
 }
 
-const hallImage = ref<HTMLImageElement>()
+export type Game = {
+  id: string
+  boothId: string
+  src: string
+}
+
+const hallImageEl = ref<HTMLImageElement>()
 const hallImageDimensions = ref<Dimensions>()
+const tooltipEls = ref<HTMLDivElement[]>()
 
 const props = defineProps<{
-  data: HallData
+  hall: Hall
+  games: Game[]
 }>()
 
-useResizeObserver(hallImage, (entries) => {
+useResizeObserver(hallImageEl, (entries) => {
   const entry = entries[0]
   const { top, left, bottom, right } = entry.contentRect
   hallImageDimensions.value = { top, left, bottom, right }
 })
 
-const virtualEl = {
-  getBoundingClientRect() {
-    return {
-      x: 200,
-      y: 200,
-      top: 200,
-      left: 500,
-      bottom: 20,
-      right: 20,
-      width: 20,
-      height: 20
+const virtualBooths = computed(() =>
+  props.hall.booths.reduce((acc, curr) => {
+    const booth = {
+      getBoundingClientRect(): ClientRectObject {
+        return {
+          x: 200,
+          y: 200,
+          top: 200,
+          left: 500,
+          bottom: 20,
+          right: 20,
+          width: 20,
+          height: 20
+        }
+      }
     }
+    acc[curr.id] = booth
+    return acc
+  }, <{ [id: string]: VirtualElement }>{})
+)
+
+watch(tooltipEls, (els) => {
+  if (!els) {
+    return
   }
-}
-const reference = ref(virtualEl)
-const floating = ref(null)
-const { floatingStyles } = useFloating(reference, floating, {
-  middleware: [autoPlacement(), shift(), flip()]
+  els.forEach(async (el, index) => {
+    const tooltipEl = els[index]
+    const { id, src, boothId } = props.games[index]
+    const booth = virtualBooths.value[boothId]
+
+    const { x, y } = await computePosition(booth, tooltipEl, {
+      middleware: [autoPlacement()]
+    })
+    Object.assign(tooltipEl.style, {
+      left: `${x}px`,
+      top: `${y}px`
+    })
+  })
 })
-const tooltipStyles = computed(() => ({ ...floatingStyles.value, width: '100px', height: '100px' }))
+
+// const tooltips = computed(() => {
+//   if (!tooltipEls.value) {
+//     return []
+//   }
+//   props.games.map(({ id, src, boothId }) => {
+//     const booth = virtualBooths.value[id]
+//     const { floatingStyles } = useFloating(booth, floating, {
+//       middleware: [autoPlacement(), shift(), flip()]
+//     })
+//     const tooltipStyles = computed(() => ({
+//       ...floatingStyles.value,
+//       width: '100px',
+//       height: '100px'
+//     }))
+//     return { key: id, src }
+//   })
+// })
+// const tooltips = computed(() => {
+//   if (!tooltipEls.value) {
+//     return []
+//   }
+//   props.games.map(({ id, src, boothId }) => {
+//     const booth = virtualBooths.value[id]
+//     const { floatingStyles } = useFloating(booth, floating, {
+//       middleware: [autoPlacement(), shift(), flip()]
+//     })
+//     const tooltipStyles = computed(() => ({
+//       ...floatingStyles.value,
+//       width: '100px',
+//       height: '100px'
+//     }))
+//     return { key: id, src }
+//   })
+// })
+
+//  tooltips.value.forEach((tooltip, index) => {
+//     const tooltipEl = tooltipRefs.value[index];
+
+//     // Create a virtual element based on the coordinates from the tooltip object
+//     const virtualElement = createVirtualElement(tooltip);
+
+//     // Use Floating UI to position the tooltip
+//     computePosition(virtualElement, tooltipEl, {
+//       middleware: [flip(), shift()],
+//     }).then(({ x, y }) => {
+//       Object.assign(tooltipEl.style, {
+//         position: 'absolute',
+//         left: `${x}px`,
+//         top: `${y}px`,
+//       });
+//     });
+//   });
 </script>
 
 <template>
-  <div ref="hallImage">
+  <div ref="hallImageEl">
     <img src="../resources/hall3.jpg" />
   </div>
   <div
-    ref="floating"
-    class="border-4 rounded bg-green-200 border-green-200 bg-cover bg-center bg-no-repeat"
-    :style="tooltipStyles"
+    ref="tooltipEls"
+    v-for="game in props.games"
+    :key="game.id"
+    class="absolute top-0 left-0 border-4 rounded bg-green-200 border-green-200 bg-cover bg-center bg-no-repeat"
+    style="width: 100px; height: 100px"
   >
-    <img src="https://tabletoptogether.com/tool/game_images/29/17537_1.jpg" />
+    <img :src="game.src" />
   </div>
 </template>
-
-<style>
-#container {
-  width: 100px; /*or 70%, or what you want*/
-  height: 200px; /*or 70%, or what you want*/
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import HallView, { HallData } from './HallView.vue'
+import HallView, { Hall, Game } from './HallView.vue'
 
-const hall3: HallData = {
+const hall3: Hall = {
   width: 3509,
   height: 4963,
-  booths: [{ id: '3A112', name: '??', top: 3812, left: 1260, bottom: 4030, right: 1422 }]
+  booths: [
+    { id: '3A112', name: '??', top: 3812, left: 1260, bottom: 4030, right: 1422 },
+    { id: '3A113', name: '??', top: 3612, left: 1060, bottom: 3830, right: 1222 }
+  ]
 }
+
+const games: Game[] = [
+  {
+    id: '1',
+    boothId: '3A112',
+    src: 'https://tabletoptogether.com/tool/game_images/29/17537_1.jpg'
+  },
+  { id: '2', boothId: '3A113', src: 'https://tabletoptogether.com/tool/game_images/29/17537_1.jpg' }
+]
 </script>
 
 <template>
-  <hall-view :data="hall3" />
+  <hall-view :hall="hall3" :games="games" />
 </template>
